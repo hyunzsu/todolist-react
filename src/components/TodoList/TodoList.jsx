e09543107874ed6d6a714170077e6aca9473ee2c
hyunzsu <kkm5169@naver.com>
2023-08-22 15:26:51 +0900

    [✨기능] 삭제하기 구현하기 💡

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,27 +1,32 @@
 import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
+import Todo from '../Todo/Todo';
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
     { id: '123', text: '장보기', status: 'active' },
     { id: '124', text: '공부하기', status: 'active' },
   ]);
-  const handleAdd = (todo) => {
-    // 새로운 투두를 todos에 업데이트 해야 함
-    console.log(todo);
-    setTodos([...todos, todo])
-  }
-  
+
+  const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((t) => t.id !== deleted.id));
+
   return (
     <section>
       <ul>
         {todos.map((item) => (
-          <li key={item.id}>{item.text}</li>
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
         ))}
       </ul>
-      <AddTodo onAdd={handleAdd}/> 
+      <AddTodo onAdd={handleAdd} />
     </section>
   );
 }
-
-// todo가 추가되면 알려주라는 콜백함수를 props로 전달, onAdd가 되면 나의 콜백함수(handleAdd)를 호출해
