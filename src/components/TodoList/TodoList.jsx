c6b7b93c5ba0431b040febe9c20493f4e3aea8e1
hyunzsu <kkm5169@naver.com>
2023-08-29 17:49:36 +0900

    [✨기능] 로컬스토리지 활용해 투두 아이템 저장하기 💡

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
-import styles from './TodoList.module.css'
+import styles from './TodoList.module.css';
 
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState([
-    { id: '123', text: '장보기', status: 'active' },
-    { id: '124', text: '공부하기', status: 'active' },
-  ]);
+  // const [todos, setTodos] = useState(readTodosFromLocalStorage());
+  const [todos, setTodos] = useState(() => readTodosFromLocalStorage())
 
   const handleAdd = (todo) => setTodos([...todos, todo]);
   const handleUpdate = (updated) =>
     setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
   const handleDelete = (deleted) =>
     setTodos(todos.filter((t) => t.id !== deleted.id));
+
+  // todos가 변경될 때마다 수행
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos)); // todos를 json으로 변환 후 그 json을 todos라는 key에 저장
+  }, [todos]);
 
   const filtered = getFilteredItems(todos, filter);
 
@@ -34,9 +37,17 @@ export default function TodoList({ filter }) {
   );
 }
 
+function readTodosFromLocalStorage() {
+  console.log('readTodosFromLocalStorage');
+  const todos = localStorage.getItem('todos'); // 로컬스토리지에서 todos를 읽어옴
+  return todos ? JSON.parse(todos) : []; // todos가 있다면 JSON.parse 이용해 배열로 만들어주고 아니면 빈배열
+}
+
 function getFilteredItems(todos, filter) {
   if (filter === 'all') {
     return todos;
   }
   return todos.filter((todo) => todo.status === filter);
 }
+
+// 객체, 배열을 변환하려면 json으로 변환 -> JSON.stringify()
