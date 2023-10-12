abb5a8b036a1cbfe9a57b2362c3290731cf3a426
hyunzsu <kkm5169@naver.com>
2023-10-12 21:04:26 +0900

    [🎸기타] .env 파일 생성

--- src/firebase-conifg.js
+++ src/firebase-conifg.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; // 코드 추가
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBFCKVabbadbK1KOdiEoTc7M-y7NuQQaH8",
-  authDomain: "login-fcd74.firebaseapp.com",
-  projectId: "login-fcd74",
-  storageBucket: "login-fcd74.appspot.com",
-  messagingSenderId: "549853458092",
-  appId: "1:549853458092:web:e5a3bda88b4fcb25c9b909"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app); // 코드 추가
+export const auth = getAuth(app);
